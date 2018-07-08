166d1f5ecf7e1660ee8d99427bd69922f3532587
Balaji Kamalesh <abalajikamalesh@gmail.com>
2018-07-08 20:09:28 +0530

    pg config added

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : process.env.DATABASE_URL,
+    connectionstring : process.env.DATABASE_URL,
     ssl: true
   }
 });
